3ddd822fc2bd6a99ae286d7fb05084807360d4af
Tomasz Trzaskoma <przytrzask@gmail.com>
2023-09-06 17:59:29 +0200

    visuals

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -3,7 +3,10 @@ import { ProductListItem } from "@/ui/molecules/ProductListItem";
 
 export const ProductList = ({ products }: { products: ProductType[] }) => {
 	return (
-		<ul className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+		<ul
+			data-testid="products-list" 
+			className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+		>
 			{products.map((product) => (
 				<ProductListItem key={product.id} product={product} />
 			))}
