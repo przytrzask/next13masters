6a30467af182b427539ef78694c303fb2e92aa97
Tomasz Trzaskoma <przytrzask@gmail.com>
2023-10-17 22:50:36 +0200

    terms and conditions

--- src/app/(static-pages)/terms/page.tsx
+++ src/app/(static-pages)/terms/page.tsx
@@ -1,8 +1,66 @@
 export default function TermsPage() {
 	return (
-		<section className="bg-slate-50">
+		<section>
 			<h2>Terms and Conditions</h2>
-			<p>Terms and conditions for TrzasQ</p>
+			<p>Welcome to TrzasQ!</p>
+
+			<h3>1. Introduction</h3>
+			<p>
+				These terms and conditions outline the rules and regulations for the use of TrzasQ website
+				and services. By accessing this website, you accept these terms and conditions in full. If
+				you disagree with any part of these terms, do not use TrzasQ services.
+			</p>
+
+			<h3>2. License</h3>
+			<p>
+				Unless otherwise stated, TrzasQ owns the intellectual property rights for all material on
+				the website. You may view and print pages from the website for your personal use, subject to
+				the restrictions set out below.
+			</p>
+
+			<h3>3. User Content</h3>
+			<p>
+				In these terms and conditions, <b>User Content</b> refers to any material (including but not
+				limited to text, images, audio, and video) that you submit to TrzasQ. By submitting content,
+				you grant TrzasQ a worldwide, irrevocable, and non-exclusive license to use and distribute
+				your User Content in any existing or future media.
+			</p>
+
+			<h3>4. Restrictions</h3>
+			<p>
+				You are expressly restricted from:
+				<ul>
+					<li>Republishing material from TrzasQ without proper attribution.</li>
+					<li>Selling or sub-licensing material from TrzasQ.</li>
+					<li>Using TrzasQ in a way that is damaging or harmful.</li>
+				</ul>
+			</p>
+
+			<h3>5. Limitation of Liability</h3>
+			<p>
+				In no event shall TrzasQ, nor its officers, directors, or employees, be liable for any
+				indirect, consequential, or special liability arising out of or in any way related to your
+				use of the TrzasQ website or services.
+			</p>
+
+			<h3>6. Revisions</h3>
+			<p>
+				TrzasQ may revise these terms and conditions from time to time. By using this website, you
+				agree to be bound by the current version of these terms and conditions.
+			</p>
+
+			<h3>7. Governing Law</h3>
+			<p>
+				These terms and conditions will be governed by and interpreted in accordance with the laws
+				of [Country/State], and any disputes relating to these terms and conditions will be subject
+				to the exclusive jurisdiction of the courts of [Country/State].
+			</p>
+
+			<h3>Contact Us</h3>
+			<p>
+				If you have any questions about these terms and conditions, please contact us at
+				[email@trzasq.com].
+			</p>
 		</section>
 	);
 }
